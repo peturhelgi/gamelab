4117e29f5c2cb3989681f344f6c3f751271b5e65
Simon Ringeisen <simon.ringeisen@gmail.com>
2018-04-21 13:28:23 +0200

    adding first instance of gamerenderer (incomplete)

---
+++ Project/Project/GameLogic/Renderer/GameRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Project.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Game.Renderer
+{
+    class GameRenderer
+    {
+        GraphicsDevice _graphicsDevice;
+        GameState _gameState;
+        SpriteBatch _spriteBatch;
+
+        public GameRenderer(GraphicsDevice graphicsDevice, GameState gameState) {
+            _graphicsDevice = graphicsDevice;
+            _gameState = gameState;
+            _spriteBatch = new SpriteBatch(_graphicsDevice);
+        }
+
+        public void Draw(GameTime gameTime, int width, int height)
+        {
+
+
+        }
+    }
+}
