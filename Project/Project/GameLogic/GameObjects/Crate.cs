dfb98a87273c2c205dcb5c6003a576a54d86fa46
hbjarni <hbjarni@student.ethz.ch>
2018-04-29 23:53:30 +0200

    crate.cs was missing

---
+++ Project/Project/GameLogic/GameObjects/Crate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.GameLogic.GameObjects
+{
+    class Crate : GameObject
+    {
+        public Crate(Vector2 position, Vector2 spriteSize, string textureString)
+        {
+
+            TextureString = textureString;
+            Position = position;
+            SpriteSize = spriteSize;
+
+            Speed = new Vector2(0);
+            Mass = 10;
+            Visible = true;
+        }
+    }
+}
