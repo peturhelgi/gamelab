92fb31d418cfbb2088b2b524227a1675be41a5a9
hbjarni <hbjarni@student.ethz.ch>
2018-05-03 23:37:53 +0200

    noninteracting lever

---
+++ Project/Project/GameLogic/GameObjects/Lever.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using TheGreatEscape.GameLogic.Util;
+
+namespace TheGreatEscape.GameLogic.GameObjects
+{
+    class Lever : GameObject
+    {
+        public bool ON;
+        public int ActivationId;
+
+
+
+        public Lever(Vector2 position, Vector2 spriteSize, string textureString, int actId)
+            : base(position, spriteSize)
+        {
+
+            TextureString = textureString;
+            Position = position;
+            SpriteSize = spriteSize;
+            Falling = false;
+
+            Speed = Vector2.Zero;
+            Mass = 10;
+            Visible = true;
+            LastUpdated = new TimeSpan();
+            Moveable = false;
+            ON = false;
+
+        }
+
+        public void Interact(List<Platform> platforms)
+        {
+            if (!ON)
+            {
+                foreach(Platform p in platforms)
+                    if (p.ActivationId == ActivationId) p.Activate = true;
+            }
+            else
+            {
+                foreach (Platform p in platforms)
+                    if (p.ActivationId == ActivationId) p.Activate = false;
+            }
+            ON = !ON;
+        }
+    }
+}
+
