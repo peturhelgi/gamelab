9b150993619a8f5449e072c4584a9a91ff47af9c
hbjarni <hbjarni@student.ethz.ch>
2018-05-06 00:28:47 +0200

    button activates platforms

--- Project/Project/GameLogic/GameEngine.cs
+++ Project/Project/GameLogic/GameEngine.cs
@@ -198,7 +198,30 @@ namespace TheGreatEscape.GameLogic
                         CalculateAndSetNewPosition(c, Vector2.Zero);
                     }
                 }
-                
+
+                if (c is Button)
+                {
+                    List<GameObject> possibleObjs = new List<GameObject>();
+                    foreach (GameObject q in actorsFirst)
+                        if (!(q is Ground)) possibleObjs.Add(q);
+                    //foreach (GameObject c in GameState.GetObjects(GameState.Handling.Solid))
+                    //{
+                    //    if (!(c is Platform))  possibleObjs.Add(c);
+                    //}
+                    //foreach (Miner m in GameState.GetActors()) possibleObjs.Add(m as GameObject);
+
+                    List<GameObject> touchingButtons = CollisionDetector.FindCollisions(c.BBox, possibleObjs);
+                    if (touchingButtons.Count > 0)
+                    {
+                        if (!(c as Button).ON) (c as Button).Interact(platforms);
+                    }
+                    else
+                    {
+                        MyDebugger.WriteLine("Nothing touches the button");
+                        if ((c as Button).ON) (c as Button).Interact(platforms);
+                    }
+                }
+
             }
 
 
---
+++ Project/Project/GameLogic/GameObjects/Button.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using TheGreatEscape.GameLogic.Util;
+
+namespace TheGreatEscape.GameLogic.GameObjects
+{
+    class Button : GameObject
+    {
+        public bool ON;
+        public int ActivationId;
+
+        public Button(Vector2 position, Vector2 spriteSize, string textureString, int actId)
+            : base(position, spriteSize)
+        {
+
+            TextureString = textureString;
+            Position = position;
+            SpriteSize = spriteSize;
+            Falling = false;
+
+            Speed = Vector2.Zero;
+            Mass = 10;
+            Visible = true;
+            LastUpdated = new TimeSpan();
+            Moveable = false;
+            ON = false;
+            ActivationId = actId;
+
+        }
+
+        public void Interact(List<Platform> platforms)
+        {
+            if (!ON)
+            {
+                foreach (Platform p in platforms)
+                    if (p.ActivationId == ActivationId) p.Activate = true;
+            }
+            else
+            {
+                foreach (Platform p in platforms)
+                    if (p.ActivationId == ActivationId) p.Activate = false;
+            }
+            ON = !ON;
+        }
+    }
+}
+
